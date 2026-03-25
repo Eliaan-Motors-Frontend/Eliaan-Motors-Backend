dc147f125db8be99192f3091357cc8c00239395d
Falcon-Miles <greenmanfalcony@gmail.com>
2026-03-25 20:56:30 +0000

    updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
+  return jwt.sign(
+    { id }, 
+    process.env.JWT_SECRET, 
+    { expiresIn: '30d' }  // This format is correct
+  );
 };
 
 module.exports = generateToken;
